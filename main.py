96c006f3a3dfe90c0fc44ae159bdca8f4ec47721
YuriiVavrynchuk <yuriyu.com@ukr.net>
2021-04-04 17:29:15 +0300

    added parser of the apache_logs file to find necessary data

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import re
+
+
 def main():
-    print("tmp")
+    date = "17/May/2015"
+    keep_word = "GET"
+    keep_status = " 200 "
+    counter = 0
+    hour = date + ':' + "\w\w"
+
+    with open("apache_logs", 'rb') as readfile:
+        checking_text = readfile.readlines()
+
+    for line in checking_text:
+        if (bytes(date, encoding='utf8') in line) and (bytes(keep_word, encoding='utf8') in line) and (bytes(keep_status, encoding='utf8') in line):
+            match = re.search(hour, str(line))
+            if (int(match.group().replace("17/May/2015:", '')) >= 1) and (int(match.group().replace("17/May/2015:", '')) <= 21):
+                counter += 1
+    print(counter)
 
 
 if __name__ == '__main__':
